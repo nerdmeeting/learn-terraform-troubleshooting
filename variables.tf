fcde3582918cfee230a492213cb257b1f1fe9943
Nerd Nathan <nathan@nerdmeeting.com>
2024-12-28 19:45:35 -0700

    Set version to 0.1.0.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+variable "project_version" {
+  description = "Version of the project"
+  type = string
+  default = "0.1.0"
+}
 
 variable "region" {
   description = "The AWS region your resources will be deployed"
